ef082a3bb4631e8ba03ad3124ca52cc1fe275c24
dantam <dtamvl@yahoo.com>
2020-07-10 10:47:19 +0700

    push2

--- config/database.php
+++ config/database.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Str;
 
+$url = parse_url(getenv('DATABASE_URL'));
+$host = $url['host']??null;
+$username = $url['user']??null;
+$password = $url['pass']??null;
+$database = substr($url['path'], 1);
+
+
 return [
 
     /*
@@ -15,7 +22,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsqlbona'),
 
     /*
     |--------------------------------------------------------------------------
@@ -74,6 +81,19 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+
+        'pgsqlbona' => [
+            'driver' => 'pgsql',
+            'host' => $host,
+            'port' => env('DB_PORT', '5432'),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
+            'charset' => 'utf8',
+            'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
